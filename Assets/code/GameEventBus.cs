d7ad776f1e371168c6d5b5b1b32d38a6281d5e9a
Eliot <fadookie@gmail.com>
2015-01-24 00:13:57 +0100

    Additive scene loader that works for both the main scene and dev scenes, it only runs once depending on which one you had open in the editor

--- Assets/code/GameEventBus.cs
+++ Assets/code/GameEventBus.cs
@@ -3,18 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GameEventBus : MonoBehaviour {
-	const int DEFAULT_NUM_GAMES = 2;
-	public MinigameBase[] initialGames = new MinigameBase[DEFAULT_NUM_GAMES]; //Size may change in the editor
+	public string[] gameNames;
 	List<MinigameBase> games;
 
 	// Use this for initialization
 	void Start () {
-		games = new List<MinigameBase>(initialGames);
-		foreach(MinigameBase game in games) {
+		Services.instance.Set<GameEventBus>(this);
+		SceneLoader sl = Services.instance.Get<SceneLoader>();
+		if (sl.scenesLoaded) {
+			onScenesLoaded();
+		} else {
+			sl.onScenesLoaded += onScenesLoaded;
+		}
+	}
+
+	void onScenesLoaded() {
+		StartCoroutine(findGameObjectsAfterDelay());
+	}
+
+	IEnumerator findGameObjectsAfterDelay() {
+		yield return new WaitForEndOfFrame();
+
+		games = new List<MinigameBase>(gameNames.Length);
+		foreach(string gameName in gameNames) {
+			GameObject gameObj = GameObject.Find(gameName);
+			print ("find object (" + gameObj + ") with name " + gameName);
+			MinigameBase game = gameObj.GetComponent<MinigameBase>();
+			games.Add(game);
 			subscribeToGame(game);
 		}
-
-		Services.instance.Set<GameEventBus>(this);
 	}
 
 	void subscribeToGame(MinigameBase game) {
---
+++ Assets/code/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	#region Event declarations
+	public delegate void SceneLoadEventHandler();
+	public event SceneLoadEventHandler onScenesLoaded;
+	#endregion
+
+	public string[] sceneList = new string[] {
+	};
+
+	public bool scenesLoaded = false;
+
+	// Use this for initialization
+	void Start () {
+		if (Services.instance.Get<SceneLoader>() != null) {
+			//Abort scene load as another scene loader is present
+			gameObject.SetActive(false);
+			return;
+		}
+
+		Services.instance.Set<SceneLoader>(this);
+
+		foreach(string sceneName in sceneList) {
+			Application.LoadLevelAdditive(sceneName);
+		}
+		scenesLoaded = true;
+		if (onScenesLoaded != null) {
+			onScenesLoaded();
+		}
+	}
+}
