6eaf4597118d4951cf3b4208d92288142d075c2c
Eliot <fadookie@gmail.com>
2015-01-23 22:54:14 +0100

    Stub out our basic architecture for cross-game communication/decoupling

---
+++ Assets/code/MinigameBase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MinigameBase : MonoBehaviour {
+	#region Event declarations
+	public delegate void MinigameScoreEventHandler(MinigameBase sender, int delta);
+	public MinigameScoreEventHandler onScoreIncrease;
+	public MinigameScoreEventHandler onScoreDecrease;
+	#endregion
+
+	int _score;
+	public int Score {
+		get {
+			return _score;
+		}
+		set {
+			_score = value;
+			if (onScoreIncrease != null) {
+				if(value > 0) {
+					onScoreIncrease(this, value);
+				} else if (value < 0) {
+					onScoreDecrease(this, value);
+				}
+			}
+		}
+	}
+
+	#region Respond callback
+	public abstract void onGoodEvent(int magnitude);
+	public abstract void onBadEvent(int magnitude);
+	#endregion
+}
---
+++ Assets/code/TestGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGame : MinigameBase {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void onGoodEvent(int magnitude) {
+	}
+
+	public override void onBadEvent(int magnitude) {
+	}
+}
