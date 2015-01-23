d23c3494d0a68b4daa887d1db4b70a8c73d3a7e1
Eliot <fadookie@gmail.com>
2015-01-23 23:28:49 +0100

    Write & debug GameEventBus

--- Assets/code/TestGame.cs
+++ Assets/code/TestGame.cs
@@ -3,18 +3,27 @@ using System.Collections;
 
 public class TestGame : MinigameBase {
 
+	public string button1 = "Fire1";
+	public string button2 = "Fire2";
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown(button1)) {
+			++Score;
+		} else if (Input.GetButtonDown(button2)) {
+			--Score;
+		}
 	}
 
 	public override void onGoodEvent(int magnitude) {
+		print(gameObject.name + " onGoodEvent mag:" + magnitude);
 	}
 
 	public override void onBadEvent(int magnitude) {
+		print(gameObject.name + " onBadEvent mag:" + magnitude);
 	}
 }
