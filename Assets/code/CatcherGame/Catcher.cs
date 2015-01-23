6e290fa79ce5bedcf84c128b190bf1b92675ae8f
Eliot <fadookie@gmail.com>
2015-01-24 00:54:00 +0100

    Skeleton for catcher game components; make a cube move within a range

---
+++ Assets/code/CatcherGame/Catcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Catcher : MinigameBase {
+
+	public string leftButton = "Fire1";
+	public string rightButton = "Fire3";
+
+	public float moveVel = 1f;
+	public GameObject xMin;
+	public GameObject xMax;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = transform.localPosition;
+		if (Input.GetButton(leftButton)) {
+			newPos.x -= moveVel; 
+		} else if (Input.GetButton(rightButton)) {
+			newPos.x += moveVel; 
+		}
+		newPos.x = Mathf.Clamp(newPos.x, xMin.transform.localPosition.x, xMax.transform.localPosition.x);
+		transform.localPosition = newPos; 
+	}
+
+	public override void onGoodEvent(int magnitude) {
+		print(gameObject.name + " onGoodEvent mag:" + magnitude);
+	}
+
+	public override void onBadEvent(int magnitude) {
+		print(gameObject.name + " onBadEvent mag:" + magnitude);
+	}
+}
