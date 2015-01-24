746b0ac18e84f387d27e1029923b092c28c08387
Jeroen Matser <mail@jmatser.com>
2015-01-24 22:27:59 +0100

    Updated the robot in the RunnerGame.

---
+++ Assets/code/RunnerGame/RunnerGameRotateHead.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerGameRotateHead : MonoBehaviour {
+
+	public float maxAngle = 20f;
+	public float animationTime = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float factor = Mathf.Sin(Time.realtimeSinceStartup * Mathf.PI * 2.0f / animationTime);
+		transform.localRotation = Quaternion.Euler(270f, factor * maxAngle, 0f);
+	}
+}
