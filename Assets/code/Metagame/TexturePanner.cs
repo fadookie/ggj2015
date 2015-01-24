2a37faffe1c8901260161854ac5242d3ca3d0616
Eliot <fadookie@gmail.com>
2015-01-24 23:39:24 +0100

    Quick texture panner script for artists

---
+++ Assets/code/Metagame/TexturePanner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class TexturePanner : MonoBehaviour {
+
+	public Vector2 panVelocity = Vector2.zero;
+
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = renderer.material.GetTextureOffset("_MainTex");
+		offset += panVelocity * Time.deltaTime;
+		renderer.material.SetTextureOffset("_MainTex", offset);
+	}
+}
