05c6f169dd5ab176a0987476053309efdfce68de
Jeroen Matser <mail@jmatser.com>
2015-01-24 14:08:59 +0100

    Updated running game.

--- Assets/code/RunnerGame/RunnerGamePlayer.cs
+++ Assets/code/RunnerGame/RunnerGamePlayer.cs
@@ -3,16 +3,78 @@ using System.Collections;
 
 public class RunnerGamePlayer : MonoBehaviour {
 
+	const int FRAMES_NO_COLLISION_CONSIDERED_VALID = 4;
+	public float jumpBoostTime = 0.5f;
+	public float jumpBoost = 10.0f;
+
+	int collisionCount = 0;
+	int framesNoCollision = 0;
+
+	bool onTheGround = false;
+	float currentJumpBoost = 0;
 	// Use this for initialization
 	void Start () {
-	
+		currentJumpBoost = jumpBoostTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(collisionCount <= 0)
+		{
+			framesNoCollision++;
+		}
+		else
+		{
+			framesNoCollision = 0;
+		}
+
+		onTheGround = framesNoCollision < FRAMES_NO_COLLISION_CONSIDERED_VALID;
+
+		if (onTheGround)
+		{
+			currentJumpBoost = jumpBoostTime;
+		}
+
 		if (Input.GetKeyDown(KeyCode.W))
 		{
-			rigidbody.AddForce(new Vector3(0.0f, 300.0f, 0.0f));
+			if (onTheGround)
+			{
+				rigidbody.velocity = new Vector3(0f, jumpBoost, 0f);
+				framesNoCollision = FRAMES_NO_COLLISION_CONSIDERED_VALID;
+			}
+			else
+			{
+				Debug.Log("Could not jump: framesNoCollision: " + framesNoCollision);
+			}
 		}
+
+#if true
+		if (!onTheGround) 
+		{
+			if (Input.GetKey(KeyCode.W))
+			{
+				if (currentJumpBoost > 0f)
+				{
+					currentJumpBoost -= Time.fixedDeltaTime;
+					rigidbody.velocity = new Vector3(0f, jumpBoost, 0f);
+				}
+			}
+			else
+			{
+				currentJumpBoost = 0f;
+			}
+
+		}
+#endif
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		collisionCount++;
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		collisionCount--;
 	}
 }
--- Assets/code/RunnerGame/RunnerGameWorld.cs
+++ Assets/code/RunnerGame/RunnerGameWorld.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 
 public class RunnerGameWorld : MonoBehaviour {
 
+	public Transform startBuilding;
 	public Transform[] largeBuildings;
 	public float velocity = 1.0f;
 	public float velocityIncrease = 0.1f;
 	public float activeArea = 10.0f;
+
 
 	List<Transform> activeBuildings;
 	List<Transform> toBeDeleted;
@@ -18,10 +20,16 @@ public class RunnerGameWorld : MonoBehaviour {
 		toBeDeleted = new List<Transform>();
 		toBeAdded = new List<Transform>();
 
-		SpawnNewBuilding(new Vector3(0f, -1f, 0f));
-		SpawnNewBuilding(new Vector3(5f, -1f, 0f));
 		SpawnNewBuilding(new Vector3(10f, -1f, 0f));
 		SpawnNewBuilding(new Vector3(15f, -1f, 0f));
+		SpawnNewBuilding(new Vector3(20f, -1f, 0f));
+		SpawnNewBuilding(new Vector3(25f, -1f, 0f));
+
+		foreach(var building in toBeAdded)
+		{
+			activeBuildings.Add(building);
+		}
+		toBeAdded.Clear();
 	}
 	
 	// Update is called once per frame
@@ -38,12 +46,28 @@ public class RunnerGameWorld : MonoBehaviour {
 			Vector3 pos = building.localPosition;
 			pos.x -= velocity * Time.deltaTime;
 			building.localPosition = pos;
-
 			if (pos.x <= -activeArea)
 			{
 				toBeDeleted.Add(building);
-				SpawnNewBuilding(new Vector3(activeArea, Random.Range(-1f, 1f), 0f));
 			}
+		}
+		int index = -1;
+		index = activeBuildings.Count-1;
+		float lastPos = 0;
+		if(index != -1)
+		{
+			lastPos = activeBuildings[index].localPosition.x;
+		}
+		if (lastPos <= activeArea)
+		{
+			SpawnNewBuilding(new Vector3(lastPos + Random.Range(5.0f, 6.5f), Random.Range(-1f, 1f), 0f));
+		}
+
+
+		{
+			Vector3 pos = startBuilding.localPosition;
+			pos.x -= velocity * Time.deltaTime;
+			startBuilding.localPosition = pos;
 		}
 
 		foreach(var building in toBeAdded)
@@ -55,6 +79,7 @@ public class RunnerGameWorld : MonoBehaviour {
 		foreach(var building in toBeDeleted)
 		{
 			activeBuildings.Remove(building);
+			Destroy (building.gameObject);
 		}
 		toBeDeleted.Clear();
 	}
