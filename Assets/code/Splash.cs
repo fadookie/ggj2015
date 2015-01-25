00e70d94a82e4713f1e1d40cc73a35451d9134cf
Jeroen Matser <mail@jmatser.com>
2015-01-25 13:32:26 +0100

    Added splash screen.

---
+++ Assets/code/Splash.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	public float waitTime = 5.0f;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(StartGameRoutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator StartGameRoutine()
+	{
+		yield return new WaitForSeconds(waitTime);
+		Application.LoadLevel("Main");
+	}
+}
